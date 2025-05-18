b466778a031db92c926826f068f7d4312cca8fe1
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-05-18 18:43:13 +0300

    Add cuda file

---
+++ cuda multiplication.cu
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+__global__ expressionCalculator(const float *A, const float *B, const float *C, const float *result, int matrixSize){
+}
+
+int main(){
+    float *hostA, *hostB, *hostC, *hostResult, *deviceA, *deviceB, *deviceC, *deviceResult;
+    return 0;
+}
