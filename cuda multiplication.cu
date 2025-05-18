729f3c6747bb2c822d6bcc712c987ebcecf938fc
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-05-18 18:45:29 +0300

    Update cuda file

--- cuda multiplication.cu
+++ cuda multiplication.cu
@@ -5,5 +5,13 @@ __global__ expressionCalculator(const float *A, const float *B, const float *C, 
 
 int main(){
     float *hostA, *hostB, *hostC, *hostResult, *deviceA, *deviceB, *deviceC, *deviceResult;
+
+    // Allocate memory in host memory space
+    int elements = MATRIX_SIZE * MATRIX_SIZE;
+    hostA = new float[elements];
+    hostB = new float[elements];
+    hostC = new float[elements];
+    hostResult = new float[elements];
+
     return 0;
 }
