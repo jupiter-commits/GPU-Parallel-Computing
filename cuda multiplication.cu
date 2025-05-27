33f95d9672718f5713e72dcc495208b461dd66f5
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-05-27 14:38:44 +0300

    Add device expression calculator

--- cuda multiplication.cu
+++ cuda multiplication.cu
@@ -3,6 +3,8 @@
 const int MATRIX_SIZE = 256;
 const int BLOCK_SIZE = 16;
 dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+float gridNumber = (MATRIX_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE;
+dim3 grid(gridNumber, gridNumber);
 
 __global__ matrixMultiply(const float *matA, const float *matB, const float *r, int matrixSize){
     int indexColumn = threadIdx.x + blockDimx.x * blockIdx.x;
@@ -11,6 +13,9 @@ __global__ matrixMultiply(const float *matA, const float *matB, const float *r, 
 }
 
 __global__ expressionCalculator(const float *A, const float *B, const float *C, const float *result, int matrixSize){
+	matrixMultiply<<<grid, blockSize>>>();
+	matrixMultiply<<<grid, blockSize>>>();
+	
 }
 
 int main(){
@@ -29,7 +34,7 @@ int main(){
         hostC[i] = rand();
     }
     
-     // Allocate in device memory space
+    // Allocate in device memory space
     cudaMalloc(&deviceA, elements*sizeof(float));
     cudaMalloc(&deviceB, elements*sizeof(float));
     cudaMalloc(&deviceC, elements*sizeof(float));
